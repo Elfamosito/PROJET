4757fa6030acee6df46c02693dfb7b54b6af151a
El_famosito <jeu.nathan68@gmail.com>
2024-04-05 17:13:44 +0200

    ok o k test ordi

--- Le_projet_pyxel.py
+++ Le_projet_pyxel.py
@@ -1,3 +1,11 @@
+import os
+chemin_script = os.path.dirname(os.path.abspath(__file__)) # Récupérer le chemin absolu du répertoire contenant le script Python en cours d'exécution
+nom_bibliotheque = "librairies" # Nom de la bibliothèque
+chemin_bibliotheque = os.path.join(chemin_script, nom_bibliotheque) # Stockage du chemin absolu de la bibliothèque
+
+import sys
+sys.path.append(chemin_bibliotheque) # Faire en sorte que les fichiers de chemin_bibliotheque soit lu par l'ordinateur.
+
 import pyxel as py
 import random as ra
 
@@ -58,6 +66,9 @@ class App:
         py.run(self.update, self.draw)
 
     def update(self):
+        if py.btn(py.KEY_Q):
+            py.quit()
+        
         if not self.game_over:
             self.player.update()
             self.update_obstacles()
