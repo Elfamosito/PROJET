0ffe72a752d8b3b7943d86a3f9ce5c528c37123e
El_famosito <jeu.nathan68@gmail.com>
2024-04-12 00:44:30 +0200

    genération d'obstacles aléatoires + apparition

--- le_vrai_projet.py
+++ le_vrai_projet.py
@@ -29,10 +29,14 @@ x_joueur = 0 + PLAYER_WIDTH
 y_joueur = WINDOW_HEIGHT // 2
 vivant = True
 game_over = False
-liste_obstacles =[]
+liste_obstacles_1 =[]
+liste_obstacles_2 =[]
+liste_obstacles_3 =[]
+liste_obstacles_4 =[]
 obstacle_interval = INITIAL_OBSTACLE_INTERVAL
 score = 0
 nb_minute = 0
+obstacle_genere = 0
 
 py.init(WINDOW_WIDTH,WINDOW_HEIGHT, title="Midnight Caca")
 
@@ -49,13 +53,46 @@ def Joueur():
     if py.btn(py.KEY_DOWN) and y_joueur < WINDOW_HEIGHT - PLAYER_HEIGHT :
         y_joueur += PLAYER_SPEED
 
-def Obstacle():
-    global y_obstacle, x_obstacle, WINDOW_HEIGHT, OBSTACLE_HEIGHT, WINDOW_WIDTH, OBSTACLE_WIDTH, liste_obstacles, obstacle_interval
+def Obstacle_1():
+    global y_obstacle, x_obstacle, WINDOW_HEIGHT, OBSTACLE_HEIGHT, WINDOW_WIDTH, OBSTACLE_WIDTH, liste_obstacles_1, obstacle_interval
     y_obstacle = ra.randint( 50 , WINDOW_HEIGHT - OBSTACLE_HEIGHT )
     x_obstacle = WINDOW_WIDTH + OBSTACLE_WIDTH
     
-    if py.frame_count % obstacle_interval == 0 :
-        liste_obstacles.append([x_obstacle , y_obstacle])
+    if py.frame_count % obstacle_interval == 0 and obstacle_genere == 1 :
+        liste_obstacles_1.append([x_obstacle , y_obstacle])
+        obstacle_interval -= OBSTACLE_INTERVAL_DECREMENT
+        if obstacle_interval <= 20 :
+            obstacle_interval = 15
+            
+def Obstacle_2():
+    global y_obstacle, x_obstacle, WINDOW_HEIGHT, OBSTACLE_HEIGHT, WINDOW_WIDTH, OBSTACLE_WIDTH, liste_obstacles_2, obstacle_interval
+    y_obstacle = ra.randint( 50 , WINDOW_HEIGHT - OBSTACLE_HEIGHT )
+    x_obstacle = WINDOW_WIDTH + OBSTACLE_WIDTH
+    
+    if py.frame_count % obstacle_interval == 0 and obstacle_genere == 2 :
+        liste_obstacles_2.append([x_obstacle , y_obstacle])
+        obstacle_interval -= OBSTACLE_INTERVAL_DECREMENT
+        if obstacle_interval <= 20 :
+            obstacle_interval = 15
+            
+def Obstacle_3():
+    global y_obstacle, x_obstacle, WINDOW_HEIGHT, OBSTACLE_HEIGHT, WINDOW_WIDTH, OBSTACLE_WIDTH, liste_obstacles_3, obstacle_interval
+    y_obstacle = ra.randint( 50 , WINDOW_HEIGHT - OBSTACLE_HEIGHT )
+    x_obstacle = WINDOW_WIDTH + OBSTACLE_WIDTH
+    
+    if py.frame_count % obstacle_interval == 0 and obstacle_genere == 3 :
+        liste_obstacles_3.append([x_obstacle , y_obstacle])
+        obstacle_interval -= OBSTACLE_INTERVAL_DECREMENT
+        if obstacle_interval <= 20 :
+            obstacle_interval = 15
+            
+def Obstacle_4():
+    global y_obstacle, x_obstacle, WINDOW_HEIGHT, OBSTACLE_HEIGHT, WINDOW_WIDTH, OBSTACLE_WIDTH, liste_obstacles_4, obstacle_interval
+    y_obstacle = ra.randint( 50 , WINDOW_HEIGHT - OBSTACLE_HEIGHT )
+    x_obstacle = WINDOW_WIDTH + OBSTACLE_WIDTH
+    
+    if py.frame_count % obstacle_interval == 0 and obstacle_genere == 4 :
+        liste_obstacles_4.append([x_obstacle , y_obstacle])
         obstacle_interval -= OBSTACLE_INTERVAL_DECREMENT
         if obstacle_interval <= 20 :
             obstacle_interval = 15
@@ -63,7 +100,22 @@ def Obstacle():
 def check_colision_joueur_obstacle():
     global game_over, vivant
     joueur_hitbox = [x_joueur , y_joueur , x_joueur + PLAYER_WIDTH , y_joueur + PLAYER_HEIGHT]
-    for obstacle in liste_obstacles:
+    for obstacle in liste_obstacles_1:
+        obstacle_hitbox = [obstacle[0] , obstacle[1] , obstacle[0] + OBSTACLE_WIDTH , obstacle[1] + OBSTACLE_HEIGHT ]
+        if check_hitbox_colision_joueur_obstacle(joueur_hitbox, obstacle_hitbox):
+            vivant = False
+            break
+    for obstacle in liste_obstacles_2:
+        obstacle_hitbox = [obstacle[0] , obstacle[1] , obstacle[0] + OBSTACLE_WIDTH , obstacle[1] + OBSTACLE_HEIGHT ]
+        if check_hitbox_colision_joueur_obstacle(joueur_hitbox, obstacle_hitbox):
+            vivant = False
+            break
+    for obstacle in liste_obstacles_3:
+        obstacle_hitbox = [obstacle[0] , obstacle[1] , obstacle[0] + OBSTACLE_WIDTH , obstacle[1] + OBSTACLE_HEIGHT ]
+        if check_hitbox_colision_joueur_obstacle(joueur_hitbox, obstacle_hitbox):
+            vivant = False
+            break
+    for obstacle in liste_obstacles_4:
         obstacle_hitbox = [obstacle[0] , obstacle[1] , obstacle[0] + OBSTACLE_WIDTH , obstacle[1] + OBSTACLE_HEIGHT ]
         if check_hitbox_colision_joueur_obstacle(joueur_hitbox, obstacle_hitbox):
             vivant = False
@@ -75,6 +127,7 @@ def check_hitbox_colision_joueur_obstacle(joueur_hitbox, obstacle_hitbox):
     return False
     
 def random_obstacles():
+    global obstacle_genere
     proba_type_obstacle_1 = [1 for n in range(max_1)] 
     proba_type_obstacle_2 = [2 for n in range(min_2)] 
     proba_type_obstacle_3 = [3 for n in range(min_3)]
@@ -113,30 +166,55 @@ def change_proba():
             min_3 += increase_3 # type: ignore
             min_4 += increase_4 # type: ignore
         
-    random_obstacles()
-    nb_minute += 1
-    
+            
 def update():
-    global score, nb_minute
+    global score, nb_minute, SCORE_COLOR
     
     if py.btnp(py.KEY_Q):
         py.quit()
     
-    if vivant == True :
+    if vivant :
         Joueur()
-        Obstacle()
+        Obstacle_1()
+        Obstacle_2()
+        Obstacle_3()
+        Obstacle_4()
         check_colision_joueur_obstacle()
-        for obstacle in liste_obstacles : 
+        for obstacle in liste_obstacles_1 : 
             obstacle[0] -= 10
             if obstacle[0] < -OBSTACLE_WIDTH :
-                liste_obstacles.remove(obstacle)
+                liste_obstacles_1.remove(obstacle)
                 score +=1
-                if score % 5 == 0 or nb_minute == 0:
-                    change_proba()
-                    nb_minute += 1
+        for obstacle in liste_obstacles_2 : 
+            obstacle[0] -= 10
+            if obstacle[0] < -OBSTACLE_WIDTH :
+                liste_obstacles_2.remove(obstacle)
+                score +=1
+        for obstacle in liste_obstacles_3 : 
+            obstacle[0] -= 10
+            if obstacle[0] < -OBSTACLE_WIDTH :
+                liste_obstacles_3.remove(obstacle)
+                score +=1
+        for obstacle in liste_obstacles_4 : 
+            obstacle[0] -= 10
+            if obstacle[0] < -OBSTACLE_WIDTH :
+                liste_obstacles_4.remove(obstacle)
+                score +=1
+        
+        if score % 5 == 1 or nb_minute == 0 :
+            change_proba()
+        
+        random_obstacles()
+        nb_minute += 1
                 
     else:
+        py.cls(0)
+        SCORE_COLOR += 1
+        if SCORE_COLOR == 15 :
+            SCORE_COLOR = 0
         py.text(WINDOW_WIDTH // 2 - 20, WINDOW_HEIGHT // 2, "Game Over", SCORE_COLOR)
+        
+    # nb_minute += 1
 
     
 def draw():
@@ -144,8 +222,14 @@ def draw():
     if vivant == True :
         py.cls(BACKGROUND_COLOR)
         py.blt( x_joueur , y_joueur , 0 , 0 , 0 , 150 , 53 )
-        for obstacle in liste_obstacles :
+        for obstacle in liste_obstacles_1 :
             py.rect(obstacle[0] , obstacle[1], OBSTACLE_WIDTH, OBSTACLE_HEIGHT, OBSTACLE_COLOR)
+        for obstacle in liste_obstacles_2 :
+            py.rect(obstacle[0] , obstacle[1], OBSTACLE_WIDTH+50, OBSTACLE_HEIGHT, OBSTACLE_COLOR)
+        for obstacle in liste_obstacles_3 :
+            py.rect(obstacle[0] , obstacle[1], OBSTACLE_WIDTH, OBSTACLE_HEIGHT+50, OBSTACLE_COLOR)
+        for obstacle in liste_obstacles_4 :
+            py.rect(obstacle[0] , obstacle[1], OBSTACLE_WIDTH+50, OBSTACLE_HEIGHT+50, OBSTACLE_COLOR)
         py.text(4,4,"Score: {}".format(score), SCORE_COLOR)
         
 py.run(update,draw)
